d8ee1c1bf7c8e0a62cffd9e08cb52b7682aa50d7
ashwin-g-g <ashwinsagar1410@gmail.com>
2020-01-14 17:11:12 +0530

    Solution for Euler Project Problem 2

---
+++ Even_fibonacci_numbers.py
@@ -0,0 +1,26 @@
+#EulerProject100
+#Problem 2
+#Even Fibonacci numbers
+#Solution by: Ashwin (Twitter @ashwin_g_g)
+
+def evenFiboNumberSum(limit):
+    i = 1
+    num_1 = 1
+    num_2 = 2
+    next_num = 0
+    final_sum = 0
+    for i in range(limit):
+        if num_1 % 2 == 0:
+            final_sum += num_1
+        next_num = num_1 + num_2
+        num_1 = num_2
+        num_2 = next_num
+    return final_sum
+
+def main():
+    limit = int(input("Enter the limit : "))
+    sum = evenFiboNumberSum(limit)
+    print(f"Sum of even numbers of the fibonacci series is : {sum}")
+
+if __name__ == main():
+    main()
