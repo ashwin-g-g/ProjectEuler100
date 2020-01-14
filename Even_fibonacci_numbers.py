9b6f75994438026392494d371159983e1f6c57e9
ashwin-g-g <ashwinsagar1410@gmail.com>
2020-01-14 23:03:49 +0530

    Fixed the bug in the solution

--- Even_fibonacci_numbers.py
+++ Even_fibonacci_numbers.py
@@ -4,23 +4,22 @@
 #Solution by: Ashwin (Twitter @ashwin_g_g)
 
 def evenFiboNumberSum(limit):
-    i = 1
     num_1 = 1
     num_2 = 2
     next_num = 0
     final_sum = 0
-    for i in range(limit):
+    while num_1 <= limit:
         if num_1 % 2 == 0:
             final_sum += num_1
         next_num = num_1 + num_2
         num_1 = num_2
         num_2 = next_num
     return final_sum
-
 def main():
     limit = int(input("Enter the limit : "))
     sum = evenFiboNumberSum(limit)
     print(f"Sum of even numbers of the fibonacci series is : {sum}")
+    return
 
 if __name__ == main():
     main()
